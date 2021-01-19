38e25ca27d6c2b660619282e7a5e454253ac00e6
Vivek Agate <vivekagate@gmail.com>
2021-01-18 22:31:09 -0800

    Simple service

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const handleRequest = function (request, response) {
+	  console.log(`Received request for URL: ${request.url}`);
+
+	  response.writeHead(200);
+
+	  response.end('Hello World -> Pricing Service');
+};
+
+const www = http.createServer(handleRequest);
+www.listen(8081);
